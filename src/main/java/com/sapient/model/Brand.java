4c0dc91efd4fe17df954adff48ceec608de28a26
Shreesh Pandey <er.shreeshpandey@gmail.com>
2020-12-25 18:23:27 +0000

    model class added

---
+++ src/main/java/com/sapient/model/Brand.java
@@ -0,0 +1,57 @@
+package com.sapient.model;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "B2C_BRND")
+public class Brand{
+
+    @Id
+    private Integer id;
+    private String name;
+
+    @JsonIgnoreProperties("brand")
+    @OneToMany(mappedBy = "brand",fetch = FetchType.LAZY)
+    private Set<Product> products;    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    public Brand() {
+        
+    }
+
+    
+    
+
+}
+
--- src/main/java/com/sapient/model/Product.java
+++ src/main/java/com/sapient/model/Product.java
@@ -8,6 +8,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name="B2C_PRDT")
 public class Product{
@@ -31,6 +33,91 @@ public class Product{
     @JoinColumn(insertable = false)
     private Brand brand;
 
+    @JsonIgnoreProperties("products")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(insertable = false)
+    private Color color;
+
+    @JsonIgnoreProperties("products")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(insertable = false)
     private ProductCategory productCategory;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getColorId() {
+        return colorId;
+    }
+
+    public void setColorId(Integer colorId) {
+        this.colorId = colorId;
+    }
+
+    public Integer getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Integer brandId) {
+        this.brandId = brandId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    
+    
+    
+    
 
 }
